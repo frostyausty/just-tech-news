7591768b3ad46f6f57148b84f095db68240047a7
frostyausty <priceaustin62@gmail.com>
2020-12-09 14:15:21 -0600

    added handlebar files

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
 const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,4 +23,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 //if force: true that would drop the database tables on startup
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
+    //console.log(PORT);
 });
