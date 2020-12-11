51ca385159e9bd1a6389da489b30eb607b918b3e
frostyausty <priceaustin62@gmail.com>
2020-12-11 11:01:08 -0600

    created the homepage

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,10 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
     res.status(404).end();
 });
-
-router.use('/', homeRoutes);
 
 module.exports = router;
